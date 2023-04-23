c2f5d54bd278f855428176642ea6c1c174ec12f0
Maevskaya Olga <olganma@gmail.com>
2023-04-23 17:31:41 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -15,7 +15,7 @@ function solveEquation(a, b, c) {
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let loan = amount - contribution; //S
+  let loan = amount - contribution; //S
 	let P = percent / 1200;
 	let creditPayments = loan * (P + (P / (((1 + P) ** countMonths) - 1)));
 	let totalAmount = Math.round(creditPayments * countMonths * 100) / 100;
