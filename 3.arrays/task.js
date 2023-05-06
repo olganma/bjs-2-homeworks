e4918770babffc52c09c472fa321754e35b86214
Maevskaya Olga <olganma@gmail.com>
2023-05-06 03:01:41 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+   let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, value, index, users) => {
+    acc+=value;
+    if (index === users.length - 1) {
+        return acc / users.length;
+    }
+    return acc;
+   }, 0)
+   return result;
 }
